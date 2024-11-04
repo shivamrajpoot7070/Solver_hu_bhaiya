23ccca2e96319960ae435ea99dc18c8e06413772
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2024-11-04 18:06:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,129 @@
+class Solution {
+public:
+    int compress(vector<char>& arr) {
+
+        // int ct=1;
+
+        // int sum=0;
+
+        // int ind=0;
+
+
+        // for(int i=0;i<arr.size()-1;i++){
+
+        //     if(arr[i]==arr[i+1]){
+        //         ct++;
+        //     }
+
+        //     else{
+        //         if(ct==1){
+        //             arr[ind]=arr[i];
+        //         }
+
+        //         else{
+        //             int t=ct;
+        //             int n1=0;
+        //             arr[ind]=arr[i];
+
+        //             while(t>0){
+        //                 int x=t%10;
+        //                 n1=n1*10+x;
+
+        //                 t=t/10;
+        //             }
+
+        //             while(n1>0){
+
+        //                 int x=n1%10;
+
+        //                 arr[ind]=char(x);
+        //                 ind++;
+
+        //                 n1=n1/10;
+
+        //             }
+
+        //             ct=1;
+        //         }
+
+                
+        //     }
+        // }
+
+
+        // if(ct>1){
+
+        //             int t=ct;
+        //             int n1=0;
+        //             arr[ind]=arr[arr.size()-1];
+
+        //             while(t>0){
+        //                 int x=t%10;
+        //                 n1=n1*10+x;
+
+        //                 t=t/10;
+        //             }
+
+        //             while(n1>0){
+
+        //                 int x=n1%10;
+
+        //                 arr[ind]=char(x);
+        //                 ind++;
+
+        //                 n1=n1/10;
+
+        //             }
+
+        // }
+
+        // return arr.size();
+
+
+        int ct=1;
+
+        string ans="";
+
+        if(arr.size()==1){
+            return arr.size();
+        }
+
+        for(int i=0;i<arr.size()-1;i++){
+
+            if(arr[i]==arr[i+1]){
+                ct++;
+            }
+
+            else{
+
+                if(ct==1){
+                    ans+=arr[i];
+                }
+
+                else{
+                    ans+=arr[i];
+                    ans+=to_string(ct);
+                    
+                }
+
+                ct=1;
+
+            }
+        }
+
+        if(ct>1){
+
+            ans+=arr[arr.size()-1];
+            ans+=to_string(ct);
+        }
+
+        for(int i=0;i<ans.length();i++){
+
+            arr[i]=ans[i];
+        }
+
+
+        return ans.length();
+        
+    }
+};
