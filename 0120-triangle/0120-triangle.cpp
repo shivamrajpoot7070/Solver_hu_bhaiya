fdee7f0c894fd3a34315516cb86a41d2515422ce
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2024-11-30 02:06:01 +0530

    Time: 0 ms (100%), Space: 13.2 MB (5.08%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
 
-    int find(vector<vector<int>>&arr,int n,int i,int j){
+    int find(vector<vector<int>>&arr,int n,int i,int j,vector<vector<int>>&dp){
 
 
         if(i==n-1){
             return arr[i][j];
         }
 
+        if(dp[i][j]!=-1){
 
-        int down=arr[i][j]+find(arr,n,i+1,j);
+            return dp[i][j];
+        }
 
-        int diagonal=arr[i][j]+find(arr,n,i+1,j+1);
+
+        int down=arr[i][j]+find(arr,n,i+1,j,dp);
+
+        int diagonal=arr[i][j]+find(arr,n,i+1,j+1,dp);
 
 
-        return min(down,diagonal);
+        return dp[i][j]=min(down,diagonal);
 
 
     }
@@ -23,9 +28,8 @@ public:
     int minimumTotal(vector<vector<int>>&arr) {
 
         int n=arr.size();
-
-        return find(arr,n,0,0);
-
+        vector<vector<int>> dp(n,vector<int>(n, -1));
+        return find(arr,n,0,0,dp);
 
 
     }
