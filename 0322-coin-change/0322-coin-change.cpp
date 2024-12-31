91e9eaccf04d53b5fb81c79dc3be33b52ac21c29
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2024-12-31 22:25:53 +0530

    Time: 64 ms (13.12%), Space: 49.9 MB (18.39%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    long long find(vector<int>&arr, int amount,int i){
+    long long find(vector<int>&arr, int amount,int i,vector<vector<int>>&dp){
 
 
         if(i>=arr.size()){
@@ -8,36 +8,40 @@ public:
             if(amount==0){
                 return 1;
             }
-
-            return INT_MAX;
+            return INT_MAX;  // agr out of range
         }
 
 
         if(amount==0){
-            return 1;
+            return 1;   // mil gya to 1
         }
-
 
         if(amount<0){
             return INT_MAX;
         }
 
-        long long take=1+find(arr,amount-arr[i],i);
-        long long nott=find(arr,amount,i+1);
 
-        return min(take,nott);
+        if(dp[i][amount]!=-1){
+            return dp[i][amount];
+        }
 
+        long long take=1+find(arr,amount-arr[i],i,dp);
+        long long nott=find(arr,amount,i+1,dp);
+
+        return dp[i][amount]=min(take,nott);
 
 
     }
 
     int coinChange(vector<int>&arr, int amount) {
         int i=0;
-        long long val=find(arr,amount,i);
+        int n=arr.size();
+
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        long long val=find(arr,amount,i,dp);
         if(val==INT_MAX){
             return -1;
         }
-
         return val-1;
         
     }
