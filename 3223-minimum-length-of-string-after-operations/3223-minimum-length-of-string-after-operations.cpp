8abcb1061a2a7731aff11932c55ed49522779a28
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-01-14 18:47:06 +0530

    Time: 32 ms (57.31%), Space: 32.1 MB (58.08%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.cpp
@@ -1,51 +1,68 @@
 class Solution {
 public:
-    int minimumLength(string s) {
+    int minimumLength(string arr) {
 
-      string str="";
-      int j;
-      int k;
+    //   string str="";
+    //   int j;
+    //   int k;
 
-      int ct=0;
+    //   int ct=0;
 
-      bool p=true;
+    //   bool p=true;
 
-      for(int i=1;i<s.length()-1;i++){
+    //   for(int i=1;i<s.length()-1;i++){
 
-        if(s[i]!='#'){
+    //     if(s[i]!='#'){
 
-      bool left=false;
-      bool right=false;
-      int li=-1;
-      int ri=-1;
+    //   bool left=false;
+    //   bool right=false;
+    //   int li=-1;
+    //   int ri=-1;
 
-        for(j=i-1;j>=0;j--){
-          if(s[j]==s[i]){
-            left=true;
-            li=j;
-            break;
-          }
+    //     for(j=i-1;j>=0;j--){
+    //       if(s[j]==s[i]){
+    //         left=true;
+    //         li=j;
+    //         break;
+    //       }
+    //     }
+
+
+    //     for(k=i+1;k<s.length();k++){
+    //       if(s[k]==s[i]){
+    //         right=true;
+    //         ri=k;
+    //         break;
+    //       }
+    //     }
+
+    //     if(left&&right){
+    //         ct+=2;  // deleted char count
+    //         s[li]='#';  // deleted character
+    //         s[ri]='#';  // deleted character
+    //     }
+
+    //   }
+    //   
+
+    vector<int>hash(26,0);
+
+    int del=0;
+
+
+    for(int i=0;i<arr.length();i++){
+
+        hash[arr[i]-'a']++;
+
+
+        if(hash[arr[i]-'a']>=3){
+            hash[arr[i]-'a']-=2;
+            del+=2;
         }
 
+    }
 
-        for(k=i+1;k<s.length();k++){
-          if(s[k]==s[i]){
-            right=true;
-            ri=k;
-            break;
-          }
-        }
-
-        if(left&&right){
-            ct+=2;  // deleted char count
-            s[li]='#';  // deleted character
-            s[ri]='#';  // deleted character
-        }
-
-      }
-      }
-
-      return s.length()-ct;  // total - deleted char count
+      return arr.length()-del;  // total - deleted char count
         
     }
 };
