319cbc44c4faabad7c1e72e5e7760742247858b2
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-01-17 09:57:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
+    typedef pair<int,int>p;
     int eraseOverlapIntervals(vector<vector<int>>& arr) {
 
-        sort(arr.begin(), arr.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[1] < b[1];
-        });
+        vector<p>v;
 
-        
+        for(int i=0;i<arr.size();i++){
+            int st=arr[i][0];
+            int ed=arr[i][1];
+
+            v.push_back({ed,st});
+        }
+
+        sort(v.begin(),v.end());
+
+        int currend=-1;
+
         int ct=0;
 
-        int currend=arr[0][1];
+        for(int i=0;i<v.size();i++){
 
+            int start=v[i].second;
+            int end=v[i].first;
 
-        for(int i=1;i<arr.size();i++){
-            
-            int start=arr[i][0];
-            int end=arr[i][1];
-
-            if(currend>start){
-                ct++;
-            }
-
-            else{
+            if(start>=currend){
                 currend=end;
             }
 
+            else{
+                ct++;
+            }
         }
+
 
         return ct;
         
