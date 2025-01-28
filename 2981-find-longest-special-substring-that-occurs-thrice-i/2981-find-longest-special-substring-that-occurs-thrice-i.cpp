99404dd774c08671b026b51c288426a3a9fbf6f5
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-01-28 17:11:52 +0530

    Time: 44 ms (21.26%), Space: 15.2 MB (21.66%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -2,40 +2,63 @@ class Solution {
 public:
     int maximumLength(string str) {
 
-        
-
-    unordered_map <string,int>mpp;
-
-    for(int i=0;i<str.length();i++){
-        string temp="";
-        char last=str[i];
-        for(int j=i;j<str.length();j++){
-
-            if(str[j]==last){
-                temp+=str[j];
-                mpp[temp]++;
-                last=str[j];
-            }
-            else{
-                break;
-            }
-        }
-    }
-
-        int maxi=INT_MIN;
-
-        for(auto it:mpp){
-            if(it.second>=3){
-                string temp=it.first;
-                int n=temp.length();
-                maxi=max(maxi,n);
-            }
-        }
-
-        if(maxi==INT_MIN){
+        if(str.length()<3){
             return -1;
         }
-    return maxi;
+
+
+        map<int,int>mpp;
+        int n=str.length();
+
+        for(int i=0;i<str.length();i++){
+            mpp[str[i]]++;
+        }
+
+        int ans=-1;
+
+        for(auto it:mpp){
+
+            vector<int>hash(26,0);
+
+            if(it.second>=3){
+
+                int x=it.second;
+                char ch=it.first;
+                int i=0;
+                int j=0;
+
+             while(x){
+
+                hash[ch-'a']=x;
+
+                vector<int>hash1(26,0);
+                int ct=0;
+                i=0;
+                j=0;
+                while(i<n && j<n){
+
+                    hash1[str[j]-'a']++;
+
+                    if(j-i+1==x){
+                        if(hash==hash1){
+                            ct++;
+                        }
+                        hash1[str[i]-'a']--;
+                        i++;
+                    }
+                    j++;
+                }
+
+                if(ct>=3){
+                    ans=max(ans,x);
+                }
+                x--;
+             }
+
+            }
+        }
+
+        return ans;
         
     }
 };
