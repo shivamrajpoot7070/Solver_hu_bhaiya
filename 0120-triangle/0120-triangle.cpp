9134cac9e98d4a86a66b7feaf86fa4d0c147107e
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-02-14 15:57:43 +0530

    Time: 0 ms (100%), Space: 13.1 MB (46.93%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -4,7 +4,7 @@ public:
     int find(vector<vector<int>>&arr,int n,int i,int j,vector<vector<int>>&dp){
 
 
-        if(i==n-1){
+        if(i==n-1 || j==n-1){
             return arr[i][j];
         }
 
@@ -13,11 +13,8 @@ public:
             return dp[i][j];
         }
 
-
         int down=arr[i][j]+find(arr,n,i+1,j,dp);
-
         int diagonal=arr[i][j]+find(arr,n,i+1,j+1,dp);
-
 
         return dp[i][j]=min(down,diagonal);
 
