4fef63e73fbeffe88969337c5f73db9c4204d379
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-03-03 22:40:44 +0530

    Time: 17 ms (58.73%), Space: 25.9 MB (60.43%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,55 +1,26 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+    vector<vector<string>> groupAnagrams(vector<string>& str) {
+        
+
+        vector<vector<string>>ans;
+        unordered_map<string,vector<string>>mpp;
+
+        for(int i=0;i<str.size();i++){
+
+            string temp=str[i];
+
+            sort(temp.begin(),temp.end());
 
 
-      vector<string>copy=strs;
-      vector<vector<string>>ans;
-
-
-      for(int i=0;i<copy.size();i++){
-
-        sort(copy[i].begin(),copy[i].end());
-
-      }
-
-      set<string>s;
-
-
-      for(int i=0;i<copy.size();i++){
-
-        s.insert(copy[i]);
-
-      }
-
-
-      for(auto it=s.begin();it!=s.end();it++){
-
-        vector<string>temp;
-
-
-        for(int i=0;i<copy.size();i++){
-
-
-          if(*it==copy[i]){
-
-            temp.push_back(strs[i]);
-
-          }
-
+            mpp[temp].push_back(str[i]);
         }
 
-        ans.push_back(temp);
+        for(auto it:mpp){
+            ans.push_back(it.second);
+        }
 
 
-
-
-      }
-
-      return ans;
-
-
-
-     
-     }
+        return ans;
+    }
 };
