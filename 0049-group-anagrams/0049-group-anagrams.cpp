4177a7d9cd42e01c4b7c56159ab6d8c250a68a2a
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-03-03 22:55:13 +0530

    Time: 79 ms (6.94%), Space: 37.4 MB (5.26%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,17 +3,47 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& str) {
         
 
+        // vector<vector<string>>ans;
+        // unordered_map<string,vector<string>>mpp;
+
+        // for(int i=0;i<str.size();i++){
+
+        //     string temp=str[i];
+
+        //     sort(temp.begin(),temp.end());
+
+
+        //     mpp[temp].push_back(str[i]);
+        // }
+
+        // for(auto it:mpp){
+        //     ans.push_back(it.second);
+        // }
+
+
+        // return ans;
+
+
         vector<vector<string>>ans;
+
         unordered_map<string,vector<string>>mpp;
 
         for(int i=0;i<str.size();i++){
 
+            vector<int>hash(26,0);
+
             string temp=str[i];
 
-            sort(temp.begin(),temp.end());
+            for(int j=0;j<temp.length();j++){
+                hash[temp[j]-'a']++;
+            }
 
+            string total="";
 
-            mpp[temp].push_back(str[i]);
+            for(int j=0;j<hash.size();j++){
+                total+=to_string(hash[j])+"#";
+            }
+            mpp[total].push_back(temp);
         }
 
         for(auto it:mpp){
@@ -21,6 +51,9 @@ public:
         }
 
 
+        //sort(ans.begin(),ans.end());
+
         return ans;
+
     }
 };
