44b0f47a3638a81c872f81bf64c0a110518517fa
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-03-03 22:40:39 +0530

    Time: 507 ms (5.07%), Space: 24 MB (93.82%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,48 +1,55 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& str) {
-
-        vector<pair<string,int>>v;
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
 
 
-        for(int i=0;i<str.size();i++){
-
-            string temp=str[i];
-
-            sort(temp.begin(),temp.end());
-
-            v.push_back({temp,i});
-        }
+      vector<string>copy=strs;
+      vector<vector<string>>ans;
 
 
-        sort(v.begin(),v.end());
+      for(int i=0;i<copy.size();i++){
+
+        sort(copy[i].begin(),copy[i].end());
+
+      }
+
+      set<string>s;
 
 
-        vector<vector<string>>ans;
+      for(int i=0;i<copy.size();i++){
+
+        s.insert(copy[i]);
+
+      }
+
+
+      for(auto it=s.begin();it!=s.end();it++){
 
         vector<string>temp;
 
-        temp.push_back(str[v[0].second]);
 
-        string m=v[0].first;
+        for(int i=0;i<copy.size();i++){
 
-        for(int i=1;i<v.size();i++){
 
-            if(v[i].first==m){
-                temp.push_back(str[v[i].second]);
-            }
+          if(*it==copy[i]){
 
-            else{
-                ans.push_back(temp);
-                temp.clear();
-                m=v[i].first;
-                temp.push_back(str[v[i].second]);
-            } 
+            temp.push_back(strs[i]);
+
+          }
+
         }
 
         ans.push_back(temp);
 
-        return ans;
-        
-    }
+
+
+
+      }
+
+      return ans;
+
+
+
+     
+     }
 };
