947b9c7eebafe907b5e050c93640fa1ca328e9e6
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-03-04 07:44:56 +0530

    Time: 39 ms (42.85%), Space: 16.6 MB (85.06%) - LeetHub

--- 3412-find-mirror-score-of-a-string/3412-find-mirror-score-of-a-string.cpp
+++ 3412-find-mirror-score-of-a-string/3412-find-mirror-score-of-a-string.cpp
@@ -1,67 +1,21 @@
-// class Solution {
-// public:
-//     long long calculateScore(string s) {
-//         int n = s.length();
-//         vector<int> marked(n, 0);  // Track marked indices
-//         long long score = 0;
-
-//         map<char,int>mpp;
-
-//         for (int i = 0; i < n; i++) {
-//             if (marked[i]) continue; // Skip already marked indices
-
-//             // Calculate the mirror character
-//             mpp[s[i]]=i;
-//             char mirror = 'z' - (s[i] - 'a');
-
-//             if(mpp.find(mirror)!=mpp.end()){
-//                 int j=mpp[mirror];
-//                 if(!marked[j]){
-//                     score+=i-j;
-//                     marked[j]=1;
-//                     marked[i]=1;
-//                 }
-//                 break;
-//             }
-//         }
-
-//         return score;
-//     }
-// };
-
 class Solution {
 public:
-    long long calculateScore(string s) {
-        int n = s.length();
-        vector<int> marked(n, 0);  // Track marked indices
-        long long score = 0;
+    long long calculateScore(string str) {
 
-        // Store all indices for each character
-        map<char, vector<int>> mpp;
+       unordered_map<char, vector<int>> mpp; // Store last seen index of //a char
+        long long ans = 0;
 
-        // Iterate through the string
-        for (int i = 0; i < n; i++) {
-            if (marked[i]) continue; // Skip already marked indices
+        for (int i = 0; i < str.length(); i++) {
+            char mirror = 'z' - (str[i] - 'a'); // Calculate mirror character
 
-            // Calculate the mirror character
-            char mirror = 'z' - (s[i] - 'a');
-
-            // Search for the closest unmarked index j < i with s[j] == mirror
-            
-            if(mpp.find(mirror)!=mpp.end() && mpp[mirror].size()>0){
-
-                int lastpos=mpp[mirror].back();
-                mpp[mirror].pop_back(); /// mark it used;
-    
-                score+=i-lastpos;
-            }
-            else{
-
-                mpp[s[i]].push_back(i);
+            if (mpp.find(mirror) != mpp.end() && mpp[mirror].size()>0) {
+                ans += i - mpp[mirror].back();  // Add the difference to score
+                mpp[mirror].pop_back();  // Remove the matched pair
+            } 
+            else {
+                mpp[str[i]].push_back(i);  // Store the current index of the character
             }
         }
-
-        return score;
+        return ans;
     }
 };
-
