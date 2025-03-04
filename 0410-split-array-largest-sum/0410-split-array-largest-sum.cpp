7123365454f59184d809cbc5189fbe51b67c7a4c
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-03-04 06:53:47 +0530

    Time: 0 ms (100%), Space: 10.5 MB (23.74%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,53 +1,53 @@
 class Solution {
 public:
-
-    int divide(vector<int>& arr, int k,int mid){
+    bool split(vector<int>&arr,int k,int mid){
 
         int val=mid;
-
-        int ct=1;
-
+        int div=1;
 
         for(int i=0;i<arr.size();i++){
 
-            if(arr[i]<=val){
-                val=val-arr[i];
+            if(arr[i]<=mid){
+                mid=mid-arr[i];
             }
-
             else{
-                ct++;
-                val=mid;
-                val=val-arr[i];
+                div++;
+                mid=val;
+                mid-=arr[i];
             }
         }
 
-        return ct;
+        if(div==k){
+            return true;
+        }
+        else if(div>k){
+            return false;
+        }
+        else{
+            return true;
+        }
     }
-
-
     int splitArray(vector<int>& arr, int k) {
 
         int low=*max_element(arr.begin(),arr.end());
         int high=accumulate(arr.begin(),arr.end(),0);
 
-
-        int ans=low;
+        int ans=-1;
 
         while(low<=high){
 
             int mid=(low+high)/2;
 
-            if(divide(arr,k,mid)>k){  // jo sum hai wo bhot chota hai isly k me divide nai hua jyda lgg gya
-                low=mid+1;
-            }
-
-            else{
+            if(split(arr,k,mid)){
                 ans=mid;
                 high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
 
         return ans;
-
+        
     }
 };
