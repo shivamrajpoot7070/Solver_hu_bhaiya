9a53de2a569ceb8a4ef17cc05891cdb1c97cfeae
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-03-06 11:07:39 +0530

    Time: 23 ms (16.1%), Space: 31.8 MB (24.71%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
-    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& arr) {
+
+        int n=arr.size();
+        n=n*n;
+        int tsum=(n*(n+1))/2;
+
+        set<int>st;
+
+        int a;
+        int b;
 
 
-        unordered_map <int,int>mpp;
-
-        int n=grid.size();
-        int m=grid[0].size();
+        int prsum=0;
 
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                mpp[grid[i][j]]++;
+        for(int i=0;i<arr.size();i++){
+
+            for(int j=0;j<arr[i].size();j++){
+
+                 if(st.find(arr[i][j])!=st.end()){
+                    a=arr[i][j];
+                 }
+                 else{
+                    st.insert(arr[i][j]);
+                    prsum+=arr[i][j];
+                 } 
             }
         }
 
-       // missing number
+        cout<<tsum<<" "<<prsum<<" ";
+        b=tsum-prsum;
 
-       vector<int>v(2);
+        return {a,b};
 
-       for(int i=1;i<=n*n;i++){
-
-        if(mpp.find(i)==mpp.end()){
-            v[1]=i;
-        }
-       }
-
-
-       for(auto it:mpp){
-        if(it.second>=2){
-            v[0]=it.first;
-        }
-       }
-
-
-       return v;
-
-
+        
 
         
     }
