31387cb06749c047d036e105688be86f33f8c401
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-03-22 14:31:36 +0530

    Time: 3 ms (10.17%), Space: 17.7 MB (50.1%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    int find(vector<int>& arr,int ind,vector<vector<int>>&dp){
+    int find(vector<int>& arr,int ind,vector<int>&dp){
 
 
         if(ind>=arr.size()){
             return 0;
-
         }
-        
+
+        if(dp[ind]!=-1){
+            return dp[ind];
+        }
+
         int one=arr[ind]+find(arr,ind+1,dp); 
         int two=arr[ind]+find(arr,ind+2,dp);
 
-        return min(one,two);
+        return dp[ind]=min(one,two);
 
     }
 
@@ -22,7 +25,7 @@ public:
         int mini=INT_MAX;
         int n=arr.size();
 
-        vector<vector<int>>dp(n+1,vector<int>(1000,-1));
+        vector<int>dp(n+1,-1);
 
         int zero=find(arr,0,dp);
         int one=find(arr,1,dp);
