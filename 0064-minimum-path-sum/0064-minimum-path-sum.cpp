a699c53e9944500b33685369801bf0b1a0e5f129
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-04-05 13:21:18 +0530

    Time: 1 ms (41.63%), Space: 16.4 MB (37.83%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    int find(vector<vector<int>>&arr,int i,int j,vector<vector<int>>&dp){
-
-        if(i==arr.size()-1 && j==arr[0].size()-1){
-            return arr[i][j];
-        }
+    long long summ(int i,int j,vector<vector<int>>& arr,vector<vector<int>>& dp){
 
         if(i>=arr.size() || j>=arr[0].size()){
-            return 1e9;
+            return INT_MAX;
+        }
+
+        if(i==arr.size()-1 && j==arr[i].size()-1){
+            return arr[i][j];
         }
 
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
 
-        int down=arr[i][j]+find(arr,i+1,j,dp);
-        int right=arr[i][j]+find(arr,i,j+1,dp);
 
-        dp[i][j]=min(down,right);
+        long long down=arr[i][j]+summ(i+1,j,arr,dp);
+        long long right=arr[i][j]+summ(i,j+1,arr,dp);
 
-
-        return dp[i][j];
-
+        return dp[i][j]=min(down,right);
     }
     int minPathSum(vector<vector<int>>& arr) {
-
-        int m=arr.size();
-        int n=arr[0].size();
         int i=0;
         int j=0;
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return find(arr,i,j,dp);
+        int n=arr.size();
+        int m=arr[0].size();
+
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return summ(i,j,arr,dp);
+        
     }
 };
