f98d27c508012862165cbe73a8e48800ba6bb0ca
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-04-05 13:11:32 +0530

    Time: 4 ms (7.69%), Space: 23.2 MB (5.46%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
+    long long minisum(int i,int j,vector<vector<int>>&arr,vector<vector<int>>&dp){
 
-    int find(vector<vector<int>>&arr,int n,int i,int j,vector<vector<int>>&dp){
+        if(i>=arr.size() || j>=arr[i].size()){
+            return 0;
+        }
 
-
-        if(i==n-1 || j==n-1){
+        if(i==arr.size()){
             return arr[i][j];
         }
 
         if(dp[i][j]!=-1){
-
             return dp[i][j];
         }
 
-        int down=arr[i][j]+find(arr,n,i+1,j,dp);
-        int diagonal=arr[i][j]+find(arr,n,i+1,j+1,dp);
+        long long same=arr[i][j]+minisum(i+1,j,arr,dp);
 
-        return dp[i][j]=min(down,diagonal);
+        long long next=arr[i][j]+minisum(i+1,j+1,arr,dp);
 
-
+        return dp[i][j]=min(same,next);
     }
-
-
-    int minimumTotal(vector<vector<int>>&arr) {
+    int minimumTotal(vector<vector<int>>& arr) {
+        
+        int i=0;
+        int j=0;
 
         int n=arr.size();
-        vector<vector<int>> dp(n,vector<int>(n, -1));
-        return find(arr,n,0,0,dp);
+        int m=arr[0].size();
 
+        vector<vector<int>>dp(201,vector<int>(201,-1));
 
+        return minisum(i,j,arr,dp);
     }
 };
