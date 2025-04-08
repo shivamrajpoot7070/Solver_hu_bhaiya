5bc0ddf3de19e63b9a1802a2ac5f86339b1e81ee
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-04-08 23:28:08 +0530

    Time: 8 ms (27.3%), Space: 28.3 MB (35.5%) - LeetHub

--- 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,62 +1,49 @@
 class Solution {
 public:
-    int minimumOperations(vector<int>& arr) {
-
-        unordered_map <int,int>mpp;
-
-
-        for(int i=0;i<arr.size();i++){
-            mpp[arr[i]]++;
+    int minimumOperations(vector<int>&arr) {
+        
+        unordered_map<int,int>mpp;
+        for(int x:arr){
+            mpp[x]++;
         }
-
-
-        if(arr.size()<=3){
-
-            if(mpp.size()!=arr.size()){
-                return 1;
-            }
-            else{
-                return 0;
-            }
-            
-        }
-
-        int n=arr.size();
 
         int i=0;
-        int j=n-1;
+
         int ct=0;
 
-        while(i<n && i!=j){
+        while(i<arr.size()){
 
-            if(mpp.size()<j-i+1){
+            bool dup=false;
 
-                for(int x=0;x<3;x++){
-
-                    if(i<n){
-                        mpp[arr[i]]--;
-
-                    if(mpp[arr[i]]==0){
-                        mpp.erase(arr[i]);
-                    }
-                    i++;
-
-                    }
+            for(auto it:mpp){
+                if(it.second>1){
+                    dup=true;
+                    break;
                 }
-                ct++;
             }
+            int k=3;
 
-            else{
-
+            if(dup==false){
                 return ct;
             }
 
+            ct++;
+
+            while(k-- && i<arr.size()){
+                mpp[arr[i]]--;
+
+                if(mpp[arr[i]]==0){
+                    mpp.erase(arr[i]);
+                }
+                i++;
+            }
+
+            if(i>=arr.size()){
+                return ct;
+            }
         }
 
-
-
-
         return ct;
-        
+
     }
 };
