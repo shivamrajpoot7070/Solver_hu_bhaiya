c69e28b01086514c42c516dfdbf164ff05efa695
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-04-10 09:21:33 +0530

    Time: 11 ms (63.84%), Space: 32.6 MB (10.69%) - LeetHub

--- 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -1,56 +1,61 @@
 class Solution {
 public:
     int minOperations(vector<int>& arr, int k) {
-
-        sort(arr.begin(),arr.end());
-        int n=arr.size();
-
-        int maxi=arr[n-1]-1;
-        int ct=0;
-
-        while(arr[0]!=arr[n-1]){
-            int h;
-            int i=0;
-            while(true){
-                if(arr[i]==maxi){
-                    h=arr[i];
-                    break;
-                }
-                if(i==n-1){
-                    maxi--;
-                    i=0;
-                }
-                else{
-                    i++;
-                }
-            }
-
-            for(int k=n-1;k>=0;k--){
-                if(arr[k]>h){
-                    arr[k]=h;
-                }
-            }
-            ct++;
-            maxi--;
-        }
-
-        if(arr[0]==arr[n-1]){
-            
-
-            if(arr[0]==k){
-                return ct;
-            }
-
-            if(k<arr[0]){
-                ct++;
-                return ct;
-            }
-
-
-        }
-
-        return -1;
-
         
+        unordered_map<int,int>mpp;
+
+        for(int i=0;i<arr.size();i++){
+            mpp[arr[i]]++;
+        }
+
+        sort(arr.begin(),arr.end(),greater<int>());
+
+        for(int x:arr){
+            if(x<k){
+                return -1;
+            }
+        }
+
+        int ct=0;
+        for(int i=0;i<arr.size();i++){
+
+            if(mpp.find(arr[i])!=mpp.end()){
+
+                int n=arr[i]-1;
+                while(mpp.find(n)==mpp.end() && n>=1){
+                    n--;
+                }
+
+                if(n==0){
+                    if(mpp.size()==1){
+
+                        for(auto it:mpp){
+                            if(it.first==k){
+                                return ct;
+                            }
+                            else{
+                                return ct+1;
+                            }
+                        }
+                    }
+                    else{
+                        return -1;
+                    }
+                }
+                ct++;
+                int ctofn=mpp[n];
+                int ctofcurr=mpp[arr[i]];
+
+                mpp[n]=ctofn+ctofcurr;
+
+                mpp.erase(arr[i]);
+            }
+            else{
+                continue;
+            }
+        }
+
+        return ct;
+
     }
 };
