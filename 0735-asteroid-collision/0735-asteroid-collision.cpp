ecface311241eaec6fdfe9426c5e93e044669a27
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-04-20 18:51:46 +0530

    Time: 2 ms (35.79%), Space: 22.1 MB (42.17%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -2,48 +2,48 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& arr) {
 
-        vector<int> ans;
-
         stack<int>st;
 
-        for (int i = 0; i < arr.size(); i++) {
 
-            bool flag=true;
+        for(int i=0;i<arr.size();i++){
 
-            while(st.size()>0 && st.top()>0 && arr[i]<0){
-
-                int dir=st.top()+arr[i];
-
-                if(dir<0){  // left qki jo aaya number wo -ve bhi tha or top se bda bhi to dleete krte jyga top ko
-                    st.pop();
-                }
-
-                else if(dir>0){  // agr top wla bda hai aage wle to kuch na kro bs neg ko ignore kr do
-                    flag=false;
-                    break;
-                }
-                else{
-                    st.pop();
-                    flag=false;
-                    break;
-                }
-            }
-
-
-            if(flag){
+            if(arr[i]>0){
                 st.push(arr[i]);
             }
+
+            else{
+
+                while(st.size()>0 && st.top()>0 && abs(arr[i])>st.top()){
+                    st.pop();
+                }
+
+                if(st.size()==0) {
+                    st.push(arr[i]);
+                }
+
+                else{
+                    if(st.top()<=0) st.push(arr[i]);
+
+                    else{
+                        
+                        if(st.top()==abs(arr[i])){
+                            st.pop();
+                        }
+                    }
+                }
+            }
         }
 
+        vector<int>ans;
 
         while(st.size()>0){
-
             ans.push_back(st.top());
             st.pop();
-        }
+        } 
 
-        reverse(ans.begin(),ans.end());
+        reverse(ans.begin(),ans.end());  
 
         return ans;
+
     }
 };
