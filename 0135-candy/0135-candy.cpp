45976071a12b48ea514067895fc8281ce1251388
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-04-26 16:58:00 +0530

    Time: 3 ms (28.42%), Space: 23.6 MB (11.43%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int candy(vector<int>&arr) {
+
+        int n=arr.size();
+
+        vector<int>l2r(n,1);
+        vector<int>r2l(n,1);
+
+        for(int i=1;i<arr.size();i++){
+
+            if(arr[i]>arr[i-1]){
+                l2r[i]=l2r[i-1]+1;
+            }
+        }
+
+        for(int i=n-2;i>=0;i--){
+            
+            if(arr[i]>arr[i+1]){
+                r2l[i]=r2l[i+1]+1;
+            }
+        }
+
+        int candy=0;
+
+        for(int i=0;i<n;i++){
+            candy+=max(r2l[i],l2r[i]);
+        }
+
+        return candy;
+        
+    }
+};
