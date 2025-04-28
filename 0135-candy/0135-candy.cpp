8eac3b177d58e38c53f8dab15649f06eb28a9056
shivam kumar <118832883+shivamrajpoot7070@users.noreply.github.com>
2025-04-29 01:13:57 +0530

    Time: 0 ms (100%), Space: 23.5 MB (18.23%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -7,15 +7,15 @@ public:
         vector<int>l2r(n,1);
         vector<int>r2l(n,1);
 
-        for(int i=1;i<arr.size();i++){
+        for(int i=1;i<arr.size();i++){  //  left to right side
 
             if(arr[i]>arr[i-1]){
                 l2r[i]=l2r[i-1]+1;
             }
         }
 
-        for(int i=n-2;i>=0;i--){
-            
+        for(int i=n-2;i>=0;i--){   //  right to left side
+             
             if(arr[i]>arr[i+1]){
                 r2l[i]=r2l[i+1]+1;
             }
